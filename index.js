9060c0b54acc810832322a745f48d95cccd349f1
Kutujaya <77893381+kutu48@users.noreply.github.com>
2024-05-14 21:09:55 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,235 @@
+import axios from 'axios';
+import fs from 'fs';
+
+const getAccInfo = async (token) => {
+  try {
+    const { data } = await axios.get('https://db4.onchaincoin.io/api/info', {
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Linux; Android 11; Redmi Note 8 Build/RQ3A.211001.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/120.0.6099.144 Mobile Safari/537.36',
+        Accept: 'application/json, text/plain, */*',
+        'sec-ch-ua':
+          '"Not_A Brand";v="8", "Chromium";v="120", "Android WebView";v="120"',
+        'sec-ch-ua-mobile': '?1',
+        authorization: `Bearer ${token}`,
+        'sec-ch-ua-platform': '"Android"',
+        'x-requested-with': 'org.telegram.messenger',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-dest': 'empty',
+        referer: 'https://db4.onchaincoin.io/',
+        'accept-language': 'en,en-US;q=0.9',
+        'if-none-match': 'W/"19a-Nxz/2KeXKz9hf6gVOHucjVGao4c"',
+      },
+    });
+
+    return data.user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const clickChain = async (token, clickamount) => {
+  try {
+    const { data } = await axios.post(
+      'https://db4.onchaincoin.io/api/klick/myself/click',
+      {
+        clicks: clickamount,
+      },
+      {
+        headers: {
+          'User-Agent':
+            'Mozilla/5.0 (Linux; Android 11; Redmi Note 8 Build/RQ3A.211001.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/120.0.6099.144 Mobile Safari/537.36',
+          'sec-ch-ua':
+            '"Not_A Brand";v="8", "Chromium";v="120", "Android WebView";v="120"',
+          'sec-ch-ua-mobile': '?1',
+          authorization: `Bearer ${token}`,
+          'sec-ch-ua-platform': '"Android"',
+          origin: 'https://db4.onchaincoin.io',
+          'x-requested-with': 'org.telegram.messenger',
+          'sec-fetch-site': 'same-origin',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-dest': 'empty',
+          referer: 'https://db4.onchaincoin.io/',
+          'accept-language': 'en,en-US;q=0.9',
+        },
+      }
+    );
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getToken = async (hash) => {
+  try {
+    const { data } = await axios.post(
+      'https://db4.onchaincoin.io/api/validate',
+      {
+        hash: hash,
+      },
+      {
+        headers: {
+          'User-Agent':
+            'Mozilla/5.0 (Linux; Android 14; Z832 Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.6312.40 Mobile Safari/537.36',
+          'Accept-Language': 'en-US,en;q=0.5',
+          'Accept-Encoding': 'gzip, deflate, br',
+          Referer: 'https://db4.onchaincoin.io/',
+          Origin: 'https://db4.onchaincoin.io',
+          'Sec-Fetch-Dest': 'empty',
+          'Sec-Fetch-Mode': 'no-cors',
+          'Sec-Fetch-Site': 'cross-site',
+          'Alt-Used': 'db4.onchaincoin.io',
+          Connection: 'keep-alive',
+          TE: 'trailers',
+          Pragma: 'no-cache',
+          'Cache-Control': 'no-cache',
+        },
+      }
+    );
+    console.log('success get token');
+    return data.token;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const censoredName = (name) => {
+  const nameLength = name.length;
+  const minCharactersToCensor = 3;
+  const numCharactersToCensor = Math.max(
+    minCharactersToCensor,
+    Math.floor(Math.random() * (nameLength - 1))
+  );
+  const censoredIndexes = new Set();
+  while (censoredIndexes.size < numCharactersToCensor) {
+    const index = Math.floor(Math.random() * nameLength);
+    censoredIndexes.add(index);
+  }
+  let censorChar = '*';
+  let censoredName = '';
+  for (let i = 0; i < nameLength; i++) {
+    if (censoredIndexes.has(i)) {
+      censoredName += censorChar;
+
+      censorChar = censorChar === '*' ? '~' : '*';
+    } else {
+      censoredName += name[i];
+    }
+  }
+
+  return censoredName;
+};
+
+const claimDailyEnergy = async (token, name) => {
+  try {
+    const { data } = await axios.post(
+      'https://db4.onchaincoin.io/api/boosts/energy',
+      {},
+      {
+        headers: {
+          'User-Agent':
+            'Mozilla/5.0 (Linux; Android 11; Redmi Note 8 Build/RQ3A.211001.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/120.0.6099.144 Mobile Safari/537.36',
+          'sec-ch-ua':
+            '"Not_A Brand";v="8", "Chromium";v="120", "Android WebView";v="120"',
+          'sec-ch-ua-mobile': '?1',
+          authorization: `Bearer ${token}`,
+          'sec-ch-ua-platform': '"Android"',
+          origin: 'https://db4.onchaincoin.io',
+          'x-requested-with': 'org.telegram.mdgram',
+          'sec-fetch-site': 'same-origin',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-dest': 'empty',
+          referer: 'https://db4.onchaincoin.io/boost',
+          'accept-language': 'en,en-US;q=0.9',
+        },
+      }
+    );
+    console.log(`${name} success claim daily energy refill`);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const executeTask = async () => {
+  try {
+    process.stdout.write('\x1Bc');
+    const hashFile = fs.readFileSync('req.txt', 'utf-8').replace(/\r/g, '');
+    const hashArr = hashFile.split(',').map(hash => hash.trim()).filter(hash => hash !== '');
+
+    for (const hash of hashArr) {
+      const token = await getToken(hash);
+      let r = await getAccInfo(token.toString());
+      const name = censoredName(r.fullName);
+      const energyPerClick = parseInt(r.clickLevel);
+      console.log(
+        `logged as ${name} energy ${r.energy} || ${energyPerClick} energy/click || statusBanned ${r.isBanned}`
+      );
+
+      // Delay for 1 minute
+      await new Promise(resolve => setTimeout(resolve, 1000 * 30));
+
+      let dailyEnergyRefill = 1;
+      while (dailyEnergyRefill == 1) {
+        let energy = r.energy;
+
+        while (energy >= energyPerClick) {
+          if (energy < energyPerClick) {
+            // claim energy refill r.dailyEnergyRefill
+
+            console.log(`stopped because no energy`);
+            break;
+          }
+          const maxClicks = Math.min(50, Math.floor(energy / energyPerClick));
+          const clickAmount = Math.floor(Math.random() * maxClicks) + 1;
+          if (clickAmount * energyPerClick > energy) {
+            console.log('Insufficient energy');
+            break;
+          }
+          const doClick = await clickChain(token, clickAmount);
+          console.log(
+            `success ${clickAmount} click  energy ${doClick.energy}  coins ${doClick.coins}  points ${doClick.clicks} `
+          );
+          energy = doClick.energy;
+        }
+        if (r.dailyEnergyRefill == 1) {
+          console.log(`${name} Claiming daily energy refill`);
+          await claimDailyEnergy(token.toString(), name);
+          r = await getAccInfo(token.toString());
+          energy = r.energy;
+          continue;
+        } else {
+          dailyEnergyRefill = 0;
+          console.log(
+            `${name} process done because no energy and no daily energy refill`
+          );
+          console.log();
+          continue;
+        }
+      }
+    }
+
+    return;
+  } catch (error) {
+    console.log('error');
+    console.log(error);
+  }
+};
+
+(async () => {
+  try {
+    // Execute the task immediately
+    await executeTask();
+
+    // Schedule the task to run every hour
+    setInterval(async () => {
+      await executeTask();
+    }, 1000 * 60 * 60); // 1 hour in milliseconds
+
+    return;
+  } catch (error) {
+    console.log('error');
+    console.log(error);
+  }
+})();
---
+++ readme.md
@@ -0,0 +1,22 @@
+this tool for auto clicking at game <a href='https://t.me/onchaincoin_bot?start=user_2070067098'>onchain telegram game</a>
+
+<img src='./docs/screenshot.png'/>
+
+<b>Warning: Performing this action may result in your Telegram or game account being banned. By proceeding, you acknowledge that the script creator is not responsible for any consequences.</b>
+
+# How to run
+
+setup your hash query request at req.txt (make new file),
+you can get it from request post https://db4.onchaincoin.io/api/validate and see the request data, you can change your user agent first to android
+
+example hash
+
+```
+query_id=YOUROWN
+```
+
+dont forget install the package
+
+```
+npm install
+```
